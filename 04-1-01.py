b442a1bbaa36e461b726a27984d9d74f1cc61dee
shyang-git <>
2019-10-22 20:59:04 +0900

    range 강의 업데이트

--- 04-1-01.py
+++ 04-1-01.py
@@ -1,26 +1,32 @@
 # 157쪽의 1번 문제의 답 쓰고 인증샷
 # list_a = [0, 1, 2, 3, 4, 5, 6, 7]입니다. 다음 표의 함수들을 실행했을 때 list_a의 결과가 어떻게 나오는지 적어 보세요.
 
+# extend - 리스트에 요소 추가
 list_a = [0, 1, 2, 3, 4, 5, 6, 7]
 list_a.extend(list_a)
 print("list_a.extend(list_a)", list_a)
 
+# append - 리스트에 마지막에 추가
 list_a = [0, 1, 2, 3, 4, 5, 6, 7]
 list_a.append(10)
 print("list_a.append(10)", list_a)
 
+# insert - 리스트 특정한 위치(인덱스)에 값 추가
 list_a = [0, 1, 2, 3, 4, 5, 6, 7]
 list_a.insert(3, 0)
 print("list_a.insert(3, 0)", list_a)
 
+# remove - 리스트에서 값 삭제, 하나만 삭제됨
 list_a = [0, 1, 2, 3, 4, 5, 6, 7]
 list_a.remove(3)
 print("list_a.remove(3)", list_a)
 
+# pop - 리스트에서 특정한 위치(인덱스)의 값 삭제
 list_a = [0, 1, 2, 3, 4, 5, 6, 7]
 list_a.pop(3)
 print("list_a.pop(3)", list_a)
 
+# clear - 리스트 전체 삭제
 list_a = [0, 1, 2, 3, 4, 5, 6, 7]
 list_a.clear()
 print("list_a.clear()", list_a)
--- test.py
+++ test.py
@@ -18,3 +18,20 @@ del 딕셔너리["키"]
 print()
 for key in 딕셔너리:
     print("{} : {}".format(key, 딕셔너리[key]))
+
+# 인덱스 출력의 세가지 방법
+array = [273, 52, 103, 32, 57]
+
+# 첫번째 i를 인덱스로 사용 - C언어 형태
+i = 0
+for element in array:
+    print("{} : {}".format(i, element)) 
+    i += 1
+
+# 두번째 enumerate 함수 사용 - 인덱스와 값을 리턴
+for i, element in enumerate(array):
+    print("{} : {}".format(i, element)) 
+
+# 세번째 range 함수 사용 - 인덱스를 이용
+for i in range(len(array)):
+    print("{} : {}".format(i, array[i])) 
